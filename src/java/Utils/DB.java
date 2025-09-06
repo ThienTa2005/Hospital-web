dbc9faee9cefafecc21433871322944d538358fe
ThienTa2005 <162010578+ThienTa2005@users.noreply.github.com>
2025-09-06 15:11:31 +0700

    Add files via upload

---
+++ src/java/Utils/DB.java
@@ -0,0 +1,8 @@
+import Utils.DBUtils;
+public class DB {
+    public static void main(String[] args) {
+        // In thử dữ liệu từ users và patients
+        DBUtils.printUsers();
+        DBUtils.printPatients();
+    }
+}
