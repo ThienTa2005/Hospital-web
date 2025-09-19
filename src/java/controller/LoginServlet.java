aa048b37f8b70f730198c25ec43c7da7fe34b0b2
Nguyen Dinh Dat <nguyendinhdat343@gmail.com>
2025-09-19 14:20:54 +0700

    First commit

---
+++ src/java/controller/LoginServlet.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet
+{
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+    {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        
+        PrintWriter out = res.getWriter();
+        out.println(username);
+        out.println(password);
+    }
+}
