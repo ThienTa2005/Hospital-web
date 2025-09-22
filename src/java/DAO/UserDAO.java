1c29e8bd07c247b479090e5b5e763a9ad408ab2d
BTu-newbiecod <tn150938@gmail.com>
2025-09-22 11:31:04 +0700

    thêm create,delete,edit UserDAO.java

--- src/java/DAO/UserDAO.java
+++ src/java/DAO/UserDAO.java
@@ -61,4 +61,92 @@ public class UserDAO {
         }
         return null; // sai tk/mk
     }
+    
+    public boolean isUsernameExist(String username) throws SQLException{
+        String sql="select 1 from users where username=?";
+        try(
+            Connection connection=DBUtils.getConnection();
+            PreparedStatement ps=connection.prepareStatement(sql)){
+            
+            ps.setString(1,username);
+            
+            try(ResultSet rs=ps.executeQuery())
+            {
+                return rs.next();
+            } 
+           
+        }
+        
+    }
+
+    //thêm user
+    public boolean createUser(User user) throws SQLException
+    {
+         if(!(isUsernameExist(user.getUsername())))
+        {
+                String sql="insert into users(username, password, fullname, dob, gender, phonenum, address, role) values (?,?,?,?,?,?,?,?)";
+                
+        
+                 try(
+                    Connection connection=DBUtils.getConnection();
+                    PreparedStatement ps=connection.prepareStatement(sql)){
+            
+                     ps.setString(1,user.getUsername());
+                     ps.setString(2,user.getPassword());
+                     ps.setString(3,user.getFullname());
+                     ps.setString(4,user.getDob());
+                     ps.setString(5,user.getGender());
+                     ps.setString(6,user.getPhonenum());
+                     ps.setString(7,user.getAddress());
+                     ps.setString(8,user.getRole());
+                     
+                 
+                     return ps.executeUpdate()>0;
+                }
+        }
+        System.out.println("USERNAME DA TON TAI");
+        return false;
+    }
+
+    //xóa useruser
+    public boolean deleteUser(String username) throws SQLException{
+        if(isUsernameExist(username)){
+             String sql="delete from users where username=?";
+            try(
+                Connection connection=DBUtils.getConnection();
+                PreparedStatement ps=connection.prepareStatement(sql)){
+
+                ps.setString(1,username);
+                return ps.executeUpdate()>0;
+            }
+        }
+        System.out.println("KHONG TON TAI USERNAME");
+        return false; 
+    }
+    
+    //truyen thang user moi, dung username de xac dinh doi tuong update
+    public  boolean updateUser(User user) throws SQLException{
+     if(isUsernameExist(user.getUsername())){
+         String sql="update users set password=?,fullname=?,dob=?,gender=?,phonenum=?,address=?,role=? where username=?";
+         try(
+            Connection connection=DBUtils.getConnection();
+            PreparedStatement ps=connection.prepareStatement(sql)){
+             
+             ps.setString(1,user.getPassword());
+             ps.setString(2,user.getFullname());
+             ps.setString(3,user.getDob());
+             ps.setString(4,user.getGender());
+             ps.setString(5,user.getPhonenum());
+             ps.setString(6,user.getAddress());
+             ps.setString(7,user.getRole());
+             ps.setString(8,user.getUsername());
+             
+             return ps.executeUpdate()>0;
+         }
+     }
+     return false;
+     
+    }
+    
 }
+
